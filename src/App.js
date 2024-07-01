3808041d276fe53a552bf3607cd9781260a13342
ssoony00 <superior0130@gmail.com>
2024-07-01 16:22:35 +0900

    7.1 1차 통합

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import './App.css';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Chat from "./pages/Chat";
+import Joblist from "./pages/Joblist";
+import Notifications from "./pages/Notification";
+import Social from "./pages/Social";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Social />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/joblist" element={<Joblist />} />
+          {/* <Route path="/member" element={<Member />} /> */}
+          <Route path="/notifications" element={<Notifications />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Joblist.jsx
+++ src/pages/Joblist.jsx
@@ -1,19 +1,22 @@
-// src/pages/Joblist.js
 import React from "react";
-import Banner from "../components/Job/Banner";
-import Joblist from "../components/Job/Job";
+import Header from "../components/Job/Header";
 import Profile from "../components/Job/Profile";
-import "../styles/Job/Joblist.css";
+import Banner from "../components/Job/Banner";
+import Filters from "../components/Job/Filters";
+import Joblist from "../components/Job/Job";
+import "../App.css";
 
 const JoblistPage = () => {
   return (
     <div className="app">
+      <Header />
       <div className="main">
         <div className="leftColumn">
           <Profile />
         </div>
         <div className="rightColumn">
           <Banner />
+          <Filters />
           <Joblist />
         </div>
       </div>
