ec2acd2a11d2f12495aa1565966c5700f290f737
hyeonjoo <z1108102059@gmail.com>
2024-07-10 16:34:34 +0900

    header and login join

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { LoginContext } from "../contexts/LoginContextProvider";
 import "../styles/Header.css";
 
 const Header = () => {
+  const { isLogin } = useContext(LoginContext);
+
   return (
     <header className="header">
       <div className="logo">
         NET<span>WORKING</span>
       </div>
       <nav>
-        <a href="/">홈</a>
-        <a href="/joblist">채용 공고</a>
-        <a href="/chat">메시지</a>
-        <a href="/notifications" className="active">
+        <NavLink exact to="/" activeClassName="active">
+          홈
+        </NavLink>
+        <NavLink to="/joblist" activeClassName="active">
+          채용 공고
+        </NavLink>
+        <NavLink to="/chat" activeClassName="active">
+          메시지
+        </NavLink>
+        <NavLink to="/notifications" activeClassName="active">
           알림
-        </a>
+        </NavLink>
+        <NavLink to="/login" activeClassName="active">
+          {isLogin ? "로그아웃" : "로그인"}
+        </NavLink>
+        {!isLogin && (
+          <NavLink to="/join" activeClassName="active">
+            회원가입
+          </NavLink>
+        )}
       </nav>
     </header>
   );
