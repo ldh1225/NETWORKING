276a78d1b5b442c205647f97179e07e7714d32cf
ssoony00 <superior0130@gmail.com>
2024-07-19 10:25:10 +0900

    7.19 1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Header from '../components/Header'
 import LoginForm from '../components/Login/LoginForm'
 
 const Login = () => {
   return (
     <>
-        <Header />
         <div className="container">
             <LoginForm />
         </div>
