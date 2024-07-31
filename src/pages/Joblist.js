f24ee9ae6c47480b88e6fdcae154f9a996f77890
yuseungjae <dbtmdwo153@naver.com>
2024-08-01 00:44:21 +0900

    css commiy(ysj)

--- src/pages/Joblist.js
+++ src/pages/Joblist.js
@@ -6,17 +6,18 @@ import "../styles/Job/Joblist.css";
 
 const JoblistPage = () => {
   return (
-    <div className="app">
-      <div className="main">
-        <div className="leftColumn">
-          <Profile />
-        </div>
-        <div className="rightColumn">
-          <Banner />
-          <Job />
+    <div className="joblist-page"> 
+      <div className="app">
+        <div className="main">
+          <div className="leftColumn">
+            <Profile />
+          </div>
+          <div className="rightColumn">
+            <Banner />
+            <Job />
+          </div>
         </div>
       </div>
-      
     </div>
   );
 };
